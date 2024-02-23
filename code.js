4d5ba714052e8a096ff811e4ed774038c5ee02a8
Mecylinda <134785484+Mecylinda@users.noreply.github.com>
2024-02-23 13:13:26 +0100

    updated

---
+++ code.js
@@ -0,0 +1,3 @@
+const writer = document.getElementById("type-text");
+let word = document.getElementsByTagName["span"][0];
+array = ("I am great programmer", "A great developer," "subscribe to my channel",) 
