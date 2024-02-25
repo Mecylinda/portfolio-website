892f3c72e2ac38929d6ebce1eccb2ac0e6025437
Mecylinda <134785484+Mecylinda@users.noreply.github.com>
2024-02-25 16:57:56 +0100

    more update

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
-const writer = document.getElementById("type-text");
-let word = document.getElementsByTagName["span"][0];
-array = ("I am great programmer", "A great developer," "subscribe to my channel",) 
+const typeEl = document.querySelector('.text');
+const careers = ["web developer", "Freelancer"];
+
+let careerIndex = 0;
+let characterIndex = 0;
+
+
+updateText();
+function updateText(){
+      characterIndex++;
+    typeEl.innerHTML = ` <h1>
+    I'm Mercy Godsman, a ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+  
+
+if (characterIndex === careers[careerIndex].length){
+characterIndex = 0;
+careerIndex++
+}
+if (careerIndex ===careers.length){
+    careerIndex = 0;
+}
+setTimeout(updateText, 400);
+
+};
